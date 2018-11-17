5d4eac4cd7c3a0551c161329dcde92d174164113
MashiroShina <lshinamashiro@gmail.com>
2018-11-17 22:38:34 +0900

    update

--- Assets/addf.cs
+++ Assets/addf.cs
@@ -14,14 +14,14 @@ public class addf : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-        //float h = Input.GetAxis("Mouse X");
-        //float v = Input.GetAxis("Mouse Y");
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+        //float h = Input.GetAxis("Horizontal");
+        //float v = Input.GetAxis("Vertical");
         //Debug.Log(h);
         if (Input.GetMouseButton(0))
         {
-            Debug.Log(h + " " + v);
+            //Debug.Log(h + " " + v);
 	        rb.velocity = new Vector3(h , v, 0) * 10f;
         }
 	}
